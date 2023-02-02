d02332312cc80fa3a7513f2065f930cd9bf20665
r2hz <RashidAlzarooni2002>
2023-02-02 23:19:48 +0400

    Message

--- weather-forcast-recomendation/src/App.js
+++ weather-forcast-recomendation/src/App.js
@@ -1,14 +1,14 @@
 import Home from './Pages/Home';
 import './Styles/App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-
+import ErrorPage from './Pages/ErrorPage';
 function App() {
   return (
     <BrowserRouter>
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />}/>
-
+        <Route path="*" element={<ErrorPage message={"Page Doesnt Exsit"} />}/>
       </Routes>
     </div>
     </BrowserRouter>
---
+++ weather-forcast-recomendation/src/Pages/ErrorPage.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../Styles/ErrorPage.css"
+const ErrorPage = ({message}) =>{
+
+
+    return (
+        <div className="outer">
+            <div className="inner">
+            {message}
+            </div>
+        </div>
+    )
+
+}
+export default ErrorPage;
