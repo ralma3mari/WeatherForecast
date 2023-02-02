c9282cfe88d1eb1660d3055352bdf21be9dd98a9
r2hz <RashidAlzarooni2002>
2023-02-02 22:18:30 +0400

    First react app

---
+++ weather-forcast-recomendation/src/App.js
@@ -0,0 +1,18 @@
+import Home from './Pages/Home';
+import './Styles/App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />}/>
+
+      </Routes>
+    </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
