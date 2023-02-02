32606fb651dc987b2b54624be8c5b005e8bf43eb
ralma3mari <r.alma3mari@gmail.com>
2023-02-02 23:14:35 +0400

    added error page

--- weather-forecast-recommendation/src/App.js
+++ weather-forecast-recommendation/src/App.js
@@ -1,6 +1,7 @@
 import Home from './Pages/Home';
 import './Styles/App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import ErrorPage from './Pages/ErrorPage';
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />}/>
-        
+        <Route path="*" element={<ErrorPage message={"Page Doesn't Exist"}/>}/>
 
       </Routes>
 
