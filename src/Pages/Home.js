051fac62c1c804bb6d5f802d794ada514b4541e2
ralma3mari <r.alma3mari@gmail.com>
2023-02-08 23:50:44 +0400

    Added Top Visited

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,12 +1,17 @@
 import React from "react";
+import TopSearch from "../Components/TopSearch";
 import WeatherNow from "../Components/WeatherNow";
 import HomePage from "../Data/Images/HomePage.gif"
+
 
 function Home(){
 
     return(
         <div className="home" style={{background:`url(${process.env.PUBLIC_URL+HomePage}) center center/ cover no-repeat`, height:"100%", width:"100%"}}>
                 <WeatherNow />
+                <div className="wrapper2">
+                    <TopSearch />
+                </div>
         </div>
     );
 }
