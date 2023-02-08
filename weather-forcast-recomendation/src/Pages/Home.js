d446ef6e58a440616cfb07790a5c282fca9693d9
r2hz <RashidAlzarooni2002>
2023-02-08 21:30:36 +0400

    getting current location weather

--- weather-forcast-recomendation/src/Pages/Home.js
+++ weather-forcast-recomendation/src/Pages/Home.js
@@ -1,8 +1,11 @@
 import React from "react";
+import WeatherNow from "../Componets/WeatherNow";
+import HomePage from"../Data/Images/Home.gif"
 function Home(){
+
     return (
-        <div className="home">
-            Hello
+        <div className="home" style={{background:`url(${process.env.PUBLIC_URL+HomePage}) center center/cover no-repeat`, height:"100%", width:"100%"}}>
+            <WeatherNow />
         </div>
     )
 
