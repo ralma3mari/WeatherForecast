d7d9be55c04e859a55f18e1a3b776f32b7f29dd0
ralma3mari <r.alma3mari@gmail.com>
2023-02-12 02:38:56 +0400

    fetching top cities from replit API

--- src/Components/TopSearch.js
+++ src/Components/TopSearch.js
@@ -1,38 +1,62 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../Styles/TopSearch.css";
 
 const TopSearch = () => {
+  const [highScores, setHighScores] = useState([]);
+
+    useEffect(() => {
+        fetch(process.env.REACT_APP_REPLIT_API)
+            .then(response => response.json())
+            .then(data => setHighScores(data.data));
+    }, []);
+
   return (
     <div className="topSearch">
-      <h1>Top Three Visited Cities</h1>
-      <div className="wrapper3">
-        <div className="first">
-          <div className="wrapper4">
-            <div className="rank">
-              1<sup>st</sup>
+     { highScores.length>0 ? 
+      <>
+        <h1>Top Three Visited Cities</h1>
+        <div className="wrapper3">
+          {highScores[0] && (
+            <div className="first">
+              <div className="wrapper4">
+                <div className="rank">
+                  1<sup>st</sup>
+                </div>
+                <div className="city">{highScores[0].city}, {highScores[0].country}</div>
+              </div>
             </div>
-            <div className="city">Dubai, United Arab Emirates</div>
-          </div>
+          )}
+          
+          {highScores[1] && (
+            <div className="second">
+              <div className="wrapper4">
+                <div className="rank">
+                  2<sup>nd</sup>
+                </div>
+                <div className="city">{highScores[1].city}, {highScores[1].country}</div>
+              </div>
+            </div>
+          )}
+          
+            {highScores[2] && (
+              <div className="third">
+                <div className="wrapper4">
+                  <div className="rank">
+                    3<sup>rd</sup>
+                  </div>
+                  <div className="city">{highScores[2].city}, {highScores[2].country}</div>
+                </div>
+              </div>
+            )}
+          
         </div>
+      </>:
+      <>
+        <h1>Fetching Top Three Cities...</h1>
+      </>
 
-        <div className="second">
-          <div className="wrapper4">
-            <div className="rank">
-              2<sup>nd</sup>
-            </div>
-            <div className="city">Beirut, Lebanon</div>
-          </div>
-        </div>
-
-        <div className="third">
-          <div className="wrapper4">
-            <div className="rank">
-              3<sup>rd</sup>
-            </div>
-            <div className="city">Los Angeles, United States</div>
-          </div>
-        </div>
-      </div>
+     }
+      
     </div>
   );
 };
